76b3bb3cbcd2d1299c2e8a8caf775f69b30b48df
CristianIriarte <crisrock147@hotmail.com>
2023-03-01 15:27:20 -0300

    comiit

---
+++ desarrollo.js
@@ -0,0 +1,22 @@
+
+//menu lateral
+var menu_visible= false;
+let menu = document.getElementById("nav");
+function mostrarOcultarMenu(){
+    if(menu_visible==false){//si esta oculto
+        menu.style.display = "block";
+        menu_visible = true
+    }
+    else{
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+
+let links = document.querySelectorAll("nav a");
+for (var x = 0; x <links.length;x++){
+    links[x].onclick = function(){
+        menu.style,display = "none";
+        menu_visible = false;
+    }
+}
